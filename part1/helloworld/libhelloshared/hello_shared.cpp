459dc3da1d686b8b7129ddc56cbec2453db6021c
Ampermetr123 <sb.simonov@gmail.com>
2020-11-30 09:39:54 +0300

    Revert "remove all files for review"
    This reverts commit cffc51b282cc35dfa908410a59f58d465f63ebf4.

---
+++ part1/helloworld/libhelloshared/hello_shared.cpp
@@ -0,0 +1,9 @@
+#include "hello_shared.h"
+#include "version.h"
+namespace hello_shared{
+
+int  version(){
+    return PROJECT_VERSION_PATCH;
+}
+
+} 
---
+++ part1/helloworld/libhellostatic/version.h
@@ -0,0 +1,8 @@
+/**
+ * libhellostatic version config file
+ **/
+
+#pragma once
+ 
+#define LIB_VERSION 2
+
